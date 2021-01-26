fefe407fdc97651848ef1f61ad1582f7ba9020f6
KotaKato <peony.btn@gmail.com>
2021-01-26 23:37:49 +0900

    Refactoring: kilo.c
    There are some changes

--- life.c
+++ life.c
@@ -9,6 +9,24 @@ struct life {
 #define LIFE_INIT                                                              \
   { 0, 0, NULL }
 
+void clearScreen();
+void lifeNewGame(struct life *game, int height, int width);
+void lifeFreeMem(struct life *game);
+void lifeDrawGen(struct life *game);
+
+int main() {
+  struct life game = LIFE_INIT;
+  lifeNewGame(&game, 10, 10);
+  clearScreen();
+  lifeDrawGen(&game);
+  return 0;
+}
+
+void clearScreen(){
+  printf("\033[2J");
+  printf("\033[%d;%dH" ,0,0);
+}
+
 void lifeNewGame(struct life *game, int height, int width) {
   int celllen = height * width;
   game->height = height;
@@ -19,20 +37,21 @@ void lifeNewGame(struct life *game, int height, int width) {
   }
 }
 
+void lifeFreeMem(struct life *game){
+  free(game->cell);
+}
+
 void lifeDrawGen(struct life *game) {
   int height = game->height;
   int width = game->width;
   for (int h = 0; h < height; h++) {
     for (int w = 0; w < width; w++) {
-      printf("%d", game->cell[h * 10 + w]);
+      if (game->cell[h * 10 + w] == 0) {
+        printf("□ ");
+      } else {
+        printf("■ ");
+      }
     }
     printf("\n");
   }
-}
-
-int main() {
-  struct life game = LIFE_INIT;
-  lifeNewGame(&game, 10, 10);
-  lifeDrawGen(&game);
-  return 0;
 }
