b625cd2224d7acce9bd43617a7af38524619b46b
KotaKato <peony.btn@gmail.com>
2021-01-26 23:59:28 +0900

    Define initial state

--- life.c
+++ life.c
@@ -1,3 +1,16 @@
+/*
+ _____ _   _ _____    ____    _    __  __ _____    ___  _____
+|_   _| | | | ____|  / ___|  / \  |  \/  | ____|  / _ \|  ___|
+  | | | |_| |  _|   | |  _  / _ \ | |\/| |  _|   | | | | |_
+  | | |  _  | |___  | |_| |/ ___ \| |  | | |___  | |_| |  _|
+  |_| |_| |_|_____|  \____/_/   \_\_|  |_|_____|  \___/|_|
+ _     ___ _____ _____
+| |   |_ _|  ___| ____|
+| |    | || |_  |  _|
+| |___ | ||  _| | |___
+|_____|___|_|   |_____|
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,36 +23,57 @@ struct life {
   { 0, 0, NULL }
 
 void clearScreen();
-void lifeNewGame(struct life *game, int height, int width);
+void lifeNewGame(struct life *game, int height, int width, char *initialState);
 void lifeFreeMem(struct life *game);
 void lifeDrawGen(struct life *game);
+void lifeNextGen(struct life *game);
+
+/*
+State of 1st gen
+char gen1[10*10] = {
+  0,0,0,0,0,0,0,0,0,0,
+  0,0,1,0,0,0,0,0,0,0,
+  0,0,0,1,0,0,0,0,0,0,
+  0,1,1,1,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0
+};
+*/
+char gen1[10 * 10] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 int main() {
   struct life game = LIFE_INIT;
-  lifeNewGame(&game, 10, 10);
+  lifeNewGame(&game, 10, 10, gen1);
   clearScreen();
   lifeDrawGen(&game);
+  lifeFreeMem(&game);
   return 0;
 }
 
-void clearScreen(){
+void clearScreen() {
   printf("\033[2J");
-  printf("\033[%d;%dH" ,0,0);
+  printf("\033[%d;%dH", 0, 0);
 }
 
-void lifeNewGame(struct life *game, int height, int width) {
+void lifeNewGame(struct life *game, int height, int width, char *initialState) {
   int celllen = height * width;
   game->height = height;
   game->width = width;
   game->cell = malloc(sizeof(char) * celllen);
   for (int i = 0; i < celllen; i++) {
-    game->cell[i] = 0;
+    game->cell[i] = initialState[i];
   }
 }
 
-void lifeFreeMem(struct life *game){
-  free(game->cell);
-}
+void lifeFreeMem(struct life *game) { free(game->cell); }
 
 void lifeDrawGen(struct life *game) {
   int height = game->height;
@@ -55,3 +89,5 @@ void lifeDrawGen(struct life *game) {
     printf("\n");
   }
 }
+
+void lifeNextGen(struct life *game) {}
